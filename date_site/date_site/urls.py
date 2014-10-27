3a3d5c9384b5a0372d457368bcd59d81551b4eee
Vladimir <Fejsov87@gmail.com>
2014-10-27 19:26:36 +0100

    ubacivanje index stranice bez imena aplikacije

--- date_site/date_site/urls.py
+++ date_site/date_site/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^date_site_app/', include('date_site_app.urls')),
+    url(r'', include('date_site_app.urls')),
 )
 
--- date_site/date_site_app/urls.py
+++ date_site/date_site_app/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns,url
 from date_site_app import views
 
 urlpatterns=patterns('',
-    url(r'^$',views.index,name='index'),
-                        
+    url(r'^$', views.index, name='index'),                        
 )
